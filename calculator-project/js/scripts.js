b3f38de0a9f565c56b72cfbe786b43c4a63b9ede
jimmimuriuki <jimmimuriuki@gmail.com>
2018-07-03 16:03:48 +0300

    add calculator-project

---
+++ calculator-project/js/scripts.js
@@ -0,0 +1,4 @@
+var add = function(number1, number2) {return number1 + number2; };
+var number1 = parseInt(prompt("enter a new number"));
+var number2 = parseInt(prompt("enter another new number"));
+alert(add(number1, number2));
